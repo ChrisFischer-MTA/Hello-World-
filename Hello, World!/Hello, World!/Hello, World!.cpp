dbd8319d72e238acb223d56f21975054a2834589
Chris <christopherrfischer@outlook.com>
2017-08-28 22:53:50 -0400

    Fixed print Random. Added getRandInt for later. Small touch ups.

--- Hello, World!/Hello, World!/Hello, World!.cpp
+++ Hello, World!/Hello, World!/Hello, World!.cpp
@@ -9,56 +9,54 @@
 #include <malloc.h>
 #include <Windows.h>
 
-// Sloppy but this is just easier to make.
-
-
-
-// Choose one of three random greetings.
-int getIndex() {
-	// Time isn't REALLY random, but it works here.
-	return time(NULL) % 3;
-}
 
 // Uses puts, which for a variety of reasons I feel is useless in the real world.
 void outSloppy(char* phrase) {
 	puts(phrase);
 	Sleep(200);
 }
-
 // Uses printf, which is more useful then puts but can be a security risk if you code it like I have.
 void outClean(char* phrase) {
 	printf(phrase);
 	Sleep(200);
 }
-
-void printRandom(char* phrase) {
-	srand(time(NULL));
-	int r = (rand() % 90) + 33;
-	// The root of the bug here is in the pointers. Gonna look at it with fresh eyes tommorow.
-	for (int i = 0; *phrase; ++phrase) {
-		printf("Looking for %c", *phrase);
-		while ((int)((r% 90)+33) != (int)*phrase) {
-			r = (rand() % 90) + 33;
+int getRandInt(int mod, int sum) {
+	// I waste CPU cycles by doing the function this way.
+	// But this is just easier to write and this doesn't
+	// matter.
+	return (rand() % mod) + sum;
+}
+void printRandom(char phrase[]) {
+	// Its easier just to pass in the char array.
+	printf("\n");
+	for (int i = 0; phrase[i+1] != '\0'; i++) {
+		char r = getRandInt(90, 32);
+		printf("%c", r);
+		do
+		{	printf("\b");
+			r = getRandInt(90, 32);
 			printf("%c", r);
-			Sleep(50);
-			printf("\b");
-			}
-		printf("\nEquals, %i  %i\n", (int)((r % 90) + 33, (int)*phrase));
-		printf("%c ", r);
+			Sleep(1);
+		} while (r != phrase[i]);
+		//printf("\nEquals, %i  %i\n", (int)((r % 90) + 33, (int)*phrase));
 		}
+	printf("\n");
 	}
-
-
-
-
 int main()
 {
+	// Need to do the srand to init seed program wide.
+	srand(time(NULL));
+	
+	// Declare our greetings.
 	char greetings[3][20] = { "Hello, World!\n", "'ello, Mate!\n", "'ello, Poppet!\n" };
-	char* greeting = greetings[getIndex()];
+	// Store into a pointer as it's easier to write.
+	char* greeting = greetings[getRandInt(3,0)];
+
+	// Function calls. 
 	outSloppy(greeting);
 	outClean(greeting);
 	printRandom(greeting);
-	//printf("cough %s",greeting);
+
 	char* buffer = "esd";
 	scanf(buffer);
 	return 0;
